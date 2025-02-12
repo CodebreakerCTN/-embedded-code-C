2e813faf03f59d60bfca0a5bca4387d54128101a
CodebreakerCTN <nguyencao2206@gmail.com>
2025-02-12 22:08:04 +0700

    project1

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    printf("okok\n");
+    system("PAUSE");
+    while(1);
+    
+    return EXIT_SUCCESS;
+}
